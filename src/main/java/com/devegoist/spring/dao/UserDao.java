195a649b70cd76009c32d15b490345d6a9c3c078
devegoist <devegoist@gmail.com>
2019-12-17 01:11:52 +0900

    1.2.2

--- src/main/java/com/devegoist/spring/dao/UserDao.java
+++ src/main/java/com/devegoist/spring/dao/UserDao.java
@@ -33,8 +33,7 @@ public class UserDao {
     }
 
     public void add(User user) throws Exception {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users (id, name, password) values (?, ?, ?)");
         ps.setString(1, user.getId());
@@ -48,8 +47,7 @@ public class UserDao {
     }
 
     public User get(String id) throws Exception {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -66,5 +64,11 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+        return c;
     }
 }
